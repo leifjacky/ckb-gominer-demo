4a0bd3f10b1aef032e04d2c57c03e7e96fe220de
leifjacky <leif.jacky@gmail.com>
2019-09-03 12:30:01 +0800

    fix

--- main.go
+++ main.go
@@ -22,11 +22,12 @@ func main() {
 	var url, username, password, loglevel, logfile string
 	var threads int
 	pflag.StringVarP(&url, "url", "o", "ckb.uupool.cn:10861", "stratum pool url")
-	pflag.StringVarP(&username, "username", "u", "ckb1qyq2znu0gempdahctxsm49sa9jdzq9vnka7qt9ntff.worker1", "username")
+	pflag.StringVarP(&username, "username", "u", "test.worker1", "username")
 	pflag.StringVarP(&password, "password", "x", "x", "password")
 	pflag.StringVarP(&loglevel, "loglevel", "l", "debug", "log level: info, debug, trace")
 	pflag.StringVarP(&logfile, "logfile", "f", "debug.log", "logfile path")
 	pflag.IntVarP(&threads, "threads", "t", runtime.NumCPU(), "threads")
+	pflag.Parse()
 
 	logrus.SetFormatter(&logrus.TextFormatter{
 		ForceColors:   true,
